aa3173cb2f23e2b8ebdb69c5a43eef87e333e6f7
CHENAMALLU SUGUNA <sugunachenamallu.29@gmail.com>
2025-10-28 11:30:35 +0530

    Time: 123 ms (2.58%), Space: 45 MB (8.26%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isBalanced=true;
+    public int height(TreeNode  root)
+    {
+        if(root==null) return -1;
+        int l= height(root.left);
+        int r=height(root.right);
+        if(Math.abs(l-r)>1) 
+        {
+            isBalanced=false;
+        }
+        return Math.max(l,r)+1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+}
