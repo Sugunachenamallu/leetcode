752e6eebad6512baf2dde0cd44af02bb184269f3
CHENAMALLU SUGUNA <sugunachenamallu.29@gmail.com>
2025-12-07 11:15:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (10.29%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n=nums.length;
+        int cnt=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]<nums[i-1])
+            {
+              cnt++;
+            }
+        }
+        if(nums[n-1]>nums[0])
+        {
+            cnt++;
+        }
+        return cnt<=1;
+    }
+}
